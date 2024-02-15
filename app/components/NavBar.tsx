f01156d89e51744474f90b470ee285eb200ba2bd
Sandeep Singh <snmsndhu47@gmail.com>
2024-02-15 07:05:09 -0500

    adding classname and pathname to change the color of selected page

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: "About", href: "/" },
     { label: "Contact", href: "/" },
@@ -14,7 +19,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              " hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
