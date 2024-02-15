9ecb4fd352ccc6f8a9a87347aa13c31b79191be4
Sandeep Singh <snmsndhu47@gmail.com>
2024-02-15 06:28:03 -0500

    importing the navbar in the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 const page = () => {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default page;
